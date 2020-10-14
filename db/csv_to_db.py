d011639389a464039c33bda79e7f3e72397fcb2b
Vyacheslav <vdilchenko@gmail.com>
2020-10-14 21:25:30 +0600

    faster way to load csv

--- db/csv_to_db.py
+++ db/csv_to_db.py
@@ -1,49 +1,45 @@
  #!/usr/bin/env python3
 
 import psycopg2
-import csv
 
-host = ""
-db = ""
-user = ""
+
+host = "localhost"
+db = "postgres"
+user = "ilchenkoslava"
 
 conn = psycopg2.connect(f"host={host} dbname={db} user={user}")
 cur = conn.cursor()
 
-cur.execute("CREATE SCHEMA homework")
-cur.execute("SET search_path to homework")
 cur.execute("""
-    CREATE TABLE consumer_complaints(
+    CREATE TABLE IF NOT EXISTS consumer_complaints(
     date_received date not null,
     product_name text not null,
     sub_product text,
     issue text not null,
     sub_issue text,
-    ccn text,
-    cpr text,
+    consumer_complaint_narrative text,
+    company_public_response text,
     company text not null,
-    state varchar(2),
-    zip_code varchar(5),
+    state_name varchar(2),
+    zip_code varchar(10),
     tags text,
-    ccp varchar(30) CHECK (ccp in ('', 'N/A', 'Consent provided', 'Consent not provided', 'Other')),
-    submition_type text not null,
-    date_sent date not null,
-    crtc varchar(35) CHECK (crtc in ('', 'Closed with explanation', 'Closed', 'Closed with monetary relief', 'Closed with non-monetary relief', 'Untimely response')),
+    consumer_consent_provided varchar(30),
+    CHECK (consumer_consent_provided in ('', 'N/A', 'Consent provided', 'Consent not provided', 'Other')),
+    submitted_via text not null,
+    date_sent_to_company date not null,
+    company_response_to_consumer varchar(35),
+    CHECK (company_response_to_consumer in ('', 'Closed with explanation', 'Closed', 'Closed with monetary relief', 'Closed with non-monetary relief', 'Untimely response')),
     timely_response varchar(3) CHECK (timely_response in ('Yes', 'No', '')),
     consumer_disputed varchar(3) CHECK (consumer_disputed in ('Yes', 'No', '')),
-    complaint_id numeric,
-    CONSTRAINT pk_complaint PRIMARY KEY (complaint_id)
+    complaint_id serial,
+    CONSTRAINT pk_consumer_complaint PRIMARY KEY (complaint_id)
 )
 """)
 conn.commit()
 
-with open('P9-ConsumerComplaints.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader)
-    for row in reader:
-        cur.execute(
-        "INSERT INTO consumer_complaints VALUES \
-        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-        row
-    )
+filename = "P9-ConsumerComplaints.csv"
+sql = "COPY consumer_complaints FROM STDIN DELIMITER ',' CSV HEADER"
+with open(filename, "r") as f:
+    cur.copy_expert(sql, f)
 conn.commit()
+cur.close()
